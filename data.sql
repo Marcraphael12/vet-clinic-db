9dd69b4dde56bbc0e6de4247e5d4bfc4488bb0c0
Marc raphael <marcraphael979@gmail.com>
2023-02-23 01:19:20 +0100

    data: Update the owners table

--- data.sql
+++ data.sql
@@ -14,3 +14,13 @@ VALUES
 	('Boarmon', '2005-06-07', 7, true, 20.4),
 	('Blossom', '1998-10-13', 3, true, 17.0),
 	('Ditto', '2022-05-14', 4, true, 22.0);
+
+-- update the owners table
+insert into owners (full_name, age) values
+	('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
+select * from owners
